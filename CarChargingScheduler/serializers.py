1eda58ebc7f95db025eed5f283dc4128bd67f179
Jess Hatfield <joshhatfield@qogita.com>
2024-11-13 12:14:14 +0000

    feat:  added basic serializers

--- CarChargingScheduler/serializers.py
+++ CarChargingScheduler/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 
+from CarChargingScheduler.models import Car, ChargingSlot, ChargingSchedule
+
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Car
-        field
+        model = Car
+        fields = ['is_at_home','battery_level']
+
+
+class ChargingSlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChargingSlot
+        fields = ['start_datetime', 'end_datetime']
+
+
+class ChargingScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChargingSchedule
+        fields = ['paused_until']
+
+
